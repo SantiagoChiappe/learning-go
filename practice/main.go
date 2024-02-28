492fc101ef812b7dd2e67be78a078603ffb73a3f
Santiago Chiappe <47071792+SantiagoChiappe@users.noreply.github.com>
2024-02-28 18:02:31 -0500

    String to float

--- practice/main.go
+++ practice/main.go
@@ -4,7 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const bit32 = 32
+const bit64 = 64
 
 func main() {
 
@@ -12,5 +17,14 @@ func main() {
 	fmt.Print("Enter text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), bit64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", aFloat)
+	}
 
 }
